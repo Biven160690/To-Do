7ba9453da43c5eb941f1974b3a584ef46e053810
Biven160690 <bivenmira1990@gmail.com>
2022-04-15 12:44:48 +0300

    added input and button in taskaddform component

--- src/components/form/TaskAddForm.tsx
+++ src/components/form/TaskAddForm.tsx
@@ -1,3 +1,18 @@
+import './addForm.scss';
+
 export function TaskAddForm() {
-  return <div></div>;
+  return (
+    <form>
+      <div className='addForm'>
+        <input
+          type='text'
+          placeholder='+ Add a task, press Enter to save'
+          className='addForm__input'
+        />
+        <button type='submit' className='addForm__button'>
+          Add
+        </button>
+      </div>
+    </form>
+  );
 }
