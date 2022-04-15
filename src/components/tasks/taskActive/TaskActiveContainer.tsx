6d4936052dfc8455534892dbb397b925fe32c047
Biven160690 <bivenmira1990@gmail.com>
2022-04-15 18:43:59 +0300

    made layout adaptive

--- src/components/tasks/taskActive/TaskActiveContainer.tsx
+++ src/components/tasks/taskActive/TaskActiveContainer.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
+
 import { TaskActive } from './TaskActive';
 
+import './taskActive.scss';
+
 export function TaskActiveContainer() {
-  return <TaskActive />;
+  return (
+    <React.Fragment>
+      <h3 className='task-active__total'> To do (3) </h3>
+      <TaskActive />
+    </React.Fragment>
+  );
 }
